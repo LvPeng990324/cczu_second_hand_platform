eba5ac2af6a20af6f0c74d9213bccec97ddf8c5b
LvPeng-Ubuntu <lvpeng990324@qq.com>
2019-08-16 14:18:33 +0800

    更改管理员url为manage

--- cczu_second_hand_platform/urls.py
+++ cczu_second_hand_platform/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('manage/', admin.site.urls),
     path('', include('second_hand_platform.urls')),
 ]
