764f44800921af73194e8ec365fc302f11ffcfe1
LvPeng-Ubuntu <lvpeng990324@qq.com>
2019-08-25 09:00:48 +0800

    增加本地调试路径

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,8 +1,10 @@
+# 部署时候把注释的路径切换下
 import multiprocessing
 
 bind = "127.0.0.1:8080"
 workers = multiprocessing.cpu_count() * 2 + 1
 reload = True
 daemon = True
-errorlog = '/root/workplace/cczu_second_hand_platform/logs/gunicorn_error.log'
+# errorlog = '/root/workplace/cczu_second_hand_platform/logs/gunicorn_error.log'
+errorlog = '/home/lvpeng/workplace/django/cczu_second_hand_platform/logs/gunicorn_error.log'
 proc_name = 'gunicorn_second_hand_platform_project'
